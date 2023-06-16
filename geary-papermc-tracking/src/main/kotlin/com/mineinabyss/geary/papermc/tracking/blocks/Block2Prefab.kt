a0b5482e6afa7fbb3bd8a10d44a8e5fcad535bf7
Boy <sivertpaulsen2@gmail.com>
2023-06-16 12:07:27 +0200

    fix blockMap

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/blocks/Block2Prefab.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/blocks/Block2Prefab.kt
@@ -27,6 +27,12 @@ class Block2Prefab {
 
     operator fun contains(prefabKey: PrefabKey): Boolean = prefabMap.containsValue(prefabKey)
 
+    fun entries(): Set<Map.Entry<BlockData, PrefabKey>> = prefabMap.entries
+
+    fun values(): Collection<PrefabKey> = prefabMap.values
+    fun values(blockType: SetBlock.BlockType): Collection<PrefabKey> =
+        blockMap[blockType]?.mapNotNull { prefabMap[it] } ?: emptyList()
+
     private fun createBlockMap(): MutableMap<SetBlock.BlockType, Array<BlockData>> {
         return mutableMapOf<SetBlock.BlockType, Array<BlockData>>().apply {
             // Calculates tripwire states
@@ -42,45 +48,50 @@ class Block2Prefab {
                     if (i shr 6 and 1 == 1) tripWireData.isAttached = true
                     add(i, tripWireData)
                 }
-            }.toTypedArray().let { put(SetBlock.BlockType.WIRE, it) }
+            }.toTypedArray().let { putIfAbsent(SetBlock.BlockType.WIRE, it) }
 
             // Calculates noteblock states
-            // We do 25-825 to skip PIANO at first
             arrayListOf<BlockData>().apply {
-                val noteBlockData = Material.NOTE_BLOCK.createBlockData() as NoteBlock
+                // Make the default blockstate the 0'th entry in the array
+                add(0, Material.NOTE_BLOCK.createBlockData() as NoteBlock)
+                // Start at 50 to skip PIANO, which is the default Instrument
                 for (j in 50..799) {
-                    //val id = if (blocky.config.noteBlocks.restoreNormalFunctionality && j <= 50) j + 799 else j
+                    val noteBlockData = Material.NOTE_BLOCK.createBlockData() as NoteBlock
                     noteBlockData.instrument = Instrument.getByType((j / 50 % 400).toByte()) ?: continue
 
                     noteBlockData.note = Note((j % 25))
                     noteBlockData.isPowered = j / 25 % 2 == 1
-                    add(j - 50, noteBlockData)
+                    add(j - 49, noteBlockData)
                 }
 
-                // Restore functionality bs
-                for (j in 0..49) {
+                // Adds the piano states to the end of the blockMap, excluding Note:0 due to it being the default
+                // Since the order is not really relevant and some plugins might want to use the default states,
+                // them being at the end makes it easier to skip them
+                for (j in 1..49) {
+                    val noteBlockData = Material.NOTE_BLOCK.createBlockData() as NoteBlock
                     noteBlockData.instrument = Instrument.PIANO
                     noteBlockData.note = Note((j % 25))
                     noteBlockData.isPowered = j / 25 % 2 == 1
                     add(j + 750, noteBlockData)
                 }
-            }.toTypedArray().let { put(SetBlock.BlockType.NOTEBLOCK, it) }
+            }.toTypedArray().let { putIfAbsent(SetBlock.BlockType.NOTEBLOCK, it) }
 
             // Calculates cave-vine states
             arrayListOf<BlockData>().apply {
+                add(0, Material.CAVE_VINES.createBlockData() as CaveVines)
                 for (m in 0..49) {
                     val vineData = Material.CAVE_VINES.createBlockData() as CaveVines
                     vineData.isBerries = m > 25
                     vineData.age = if (m > 25) m - 25 else m
                     add(m, vineData)
                 }
-            }.toTypedArray().let { put(SetBlock.BlockType.CAVEVINE, it) }
-        }
+            }.toTypedArray().let { putIfAbsent(SetBlock.BlockType.CAVEVINE, it) }
 
-        //Calculates slab states & stair states
-        /*for (n in 1..4) {
+            //Calculates slab states & stair states
+            /*for (n in 1..4) {
             putIfAbsent(BLOCKY_SLABS.elementAt(n - 1).createBlockData() as Slab, n)
             putIfAbsent(BLOCKY_STAIRS.elementAt(n - 1).createBlockData() as Stairs, n)
-        }*/
+            }*/
+        }
     }
 }
