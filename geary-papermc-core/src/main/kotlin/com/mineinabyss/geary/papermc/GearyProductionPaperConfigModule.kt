b555a5243564c0d6f0f033173f102806b38e752e
Boy <sivertpaulsen2@gmail.com>
2023-10-21 12:35:12 +0200

    1.20.2

--- geary-papermc-core/src/main/kotlin/com/mineinabyss/geary/papermc/GearyProductionPaperConfigModule.kt
+++ geary-papermc-core/src/main/kotlin/com/mineinabyss/geary/papermc/GearyProductionPaperConfigModule.kt
@@ -6,10 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin
 class GearyProductionPaperConfigModule(
     override val plugin: JavaPlugin,
 ) : GearyPaperConfigModule {
-    override val configHolder = config<GearyPaperConfig>("config") {
-        plugin.fromPluginPath()
-        mergeUpdates = true
-    }
+    override val configHolder = config<GearyPaperConfig>("config", plugin.dataFolder.toPath(), GearyPaperConfig(), mergeUpdates = true)
 
     override val config: GearyPaperConfig by configHolder
 }
