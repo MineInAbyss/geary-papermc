f88b12729198f4e032ec9e0fec992a203b2dc354
Danielle Voznyy <dan.voznyy@gmail.com>
2023-12-26 14:57:11 -0500

    Encode components on mob spawn

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/helpers/Helpers.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/helpers/Helpers.kt
@@ -2,6 +2,7 @@ package com.mineinabyss.geary.papermc.tracking.entities.helpers
 
 import com.mineinabyss.geary.datatypes.GearyEntity
 import com.mineinabyss.geary.helpers.entity
+import com.mineinabyss.geary.papermc.datastore.encodeComponentsTo
 import com.mineinabyss.geary.papermc.datastore.loadComponentsFrom
 import com.mineinabyss.geary.papermc.tracking.entities.components.AttemptSpawn
 import com.mineinabyss.geary.prefabs.PrefabKey
@@ -19,10 +20,13 @@ fun Location.spawnFromPrefab(prefab: PrefabKey): Result<BukkitEntity> {
 
 fun Location.spawnFromPrefab(prefab: GearyEntity, existingPDC: PersistentDataContainer? = null): Result<BukkitEntity> {
     return runCatching {
-        entity {
+        val entity = entity {
             if (existingPDC != null) loadComponentsFrom(existingPDC)
             addPrefab(prefab)
             callEvent(AttemptSpawn(this@spawnFromPrefab))
-        }.get<BukkitEntity>() ?: error("Entity was not created when spawning from prefab")
+        }
+        val bukkit = entity.get<BukkitEntity>() ?: error("Entity was not created when spawning from prefab")
+        entity.encodeComponentsTo(bukkit)
+        bukkit
     }
 }
