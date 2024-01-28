7d146711324d543d788aa0e940fdce66f6cd0168
Danielle Voznyy <dan.voznyy@gmail.com>
2024-01-28 17:46:15 -0500

    Fix: Handle case where entities are added to world before MM is enabled

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/systems/updatemobtype/ConvertToMythicMobListener.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/systems/updatemobtype/ConvertToMythicMobListener.kt
@@ -1,10 +1,12 @@
 package com.mineinabyss.geary.papermc.tracking.entities.systems.updatemobtype
 
+import com.mineinabyss.geary.papermc.gearyPaper
 import com.mineinabyss.geary.papermc.tracking.entities.components.SetMythicMob
 import com.mineinabyss.geary.papermc.tracking.entities.events.GearyEntityAddToWorldEvent
 import io.lumine.mythic.api.mobs.MythicMob
 import io.lumine.mythic.bukkit.MythicBukkit
 import io.lumine.mythic.core.constants.MobKeys
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
@@ -12,13 +14,22 @@ class ConvertToMythicMobListener : Listener {
     @EventHandler
     fun GearyEntityAddToWorldEvent.onAdd() {
         val mm = MythicBukkit.inst()
-        // If MM already encoded or loaded, we let it handle things itself
-        if (entity.persistentDataContainer.has(MobKeys.TYPE)) return
-        if (mm.mobManager.isActiveMob(entity.uniqueId)) return
 
-        if (!gearyEntity.has<SetMythicMob>()) return
-        if (gearyEntity.has<MythicMob>()) return
+        fun convert() {
+            // If MM already encoded or loaded, we let it handle things itself
+            if (entity.persistentDataContainer.has(MobKeys.TYPE)) return
+            if (mm.mobManager.isActiveMob(entity.uniqueId)) return
 
-        UpdateMob.scheduleRecreation(entity, gearyEntity)
+            if (!gearyEntity.has<SetMythicMob>()) return
+            if (gearyEntity.has<MythicMob>()) return
+
+            UpdateMob.scheduleRecreation(entity, gearyEntity)
+        }
+
+        if (mm.mobManager == null) {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(gearyPaper.plugin, {
+                convert()
+            }, 1)
+        } else convert()
     }
 }
