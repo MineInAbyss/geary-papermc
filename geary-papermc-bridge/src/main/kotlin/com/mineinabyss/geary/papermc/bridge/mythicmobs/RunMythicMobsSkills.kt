ff5c299d53874aacc4af8bf60db30a78a19f5530
Danielle Voznyy <dan.voznyy@gmail.com>
2024-02-01 18:01:32 -0500

    Support running any mechanics like mm test mechanic via action

--- geary-papermc-bridge/src/main/kotlin/com/mineinabyss/geary/papermc/bridge/mythicmobs/RunMythicMobsSkills.kt
+++ geary-papermc-bridge/src/main/kotlin/com/mineinabyss/geary/papermc/bridge/mythicmobs/RunMythicMobsSkills.kt
@@ -1,13 +1,19 @@
 package com.mineinabyss.geary.papermc.bridge.mythicmobs
 
+import com.google.common.collect.Lists
 import com.mineinabyss.geary.serialization.serializers.InnerSerializer
 import com.mineinabyss.geary.systems.GearyListener
 import com.mineinabyss.geary.systems.accessors.Pointers
 import com.mineinabyss.idofront.typealiases.BukkitEntity
+import io.lumine.mythic.api.adapters.AbstractEntity
+import io.lumine.mythic.bukkit.BukkitAdapter
 import io.lumine.mythic.bukkit.MythicBukkit
+import io.lumine.mythic.core.skills.SkillMetadataImpl
+import io.lumine.mythic.core.skills.SkillTriggers
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.builtins.serializer
+import kotlin.jvm.optionals.getOrNull
 
 @Serializable(with= RunMythicMobsSkills.Serializer::class)
 class RunMythicMobsSkills(
@@ -27,7 +33,20 @@ class RunMMSkillSystem: GearyListener() {
 
     override fun Pointers.handle() {
         skill.keys.forEach {
-            MythicBukkit.inst().apiHelper.castSkill(bukkit, it)
+            val line = "[ - $it ]"
+            val entity = BukkitAdapter.adapt(bukkit)
+            val caster = MythicBukkit.inst().skillManager.getCaster(entity)
+            val skill = MythicBukkit.inst().skillManager.getSkill(line).getOrNull()
+            val meta = SkillMetadataImpl(
+                SkillTriggers.API,
+                caster,
+                entity,
+                entity.location,
+                Lists.newArrayList(*arrayOf<AbstractEntity>(entity)),
+                null,
+                1.0f
+            )
+            skill?.execute(meta)
         }
     }
 }
