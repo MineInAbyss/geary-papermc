a1ccd37b2e277fdcab0357f18573c73733deef29
Danielle Voznyy <dan.voznyy@gmail.com>
2024-02-01 21:34:02 -0500

    Reload existing mobs on prefab reread

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/systems/updatemobtype/UpdateMob.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/systems/updatemobtype/UpdateMob.kt
@@ -1,12 +1,22 @@
 package com.mineinabyss.geary.papermc.tracking.entities.systems.updatemobtype
 
 import com.mineinabyss.geary.datatypes.GearyEntity
+import com.mineinabyss.geary.papermc.datastore.encodeComponentsTo
 import com.mineinabyss.geary.papermc.gearyPaper
+import com.mineinabyss.geary.papermc.tracking.entities.gearyMobs
 import com.mineinabyss.geary.papermc.tracking.entities.helpers.spawnFromPrefab
+import com.mineinabyss.geary.papermc.tracking.entities.toGearyOrNull
 import com.mineinabyss.idofront.typealiases.BukkitEntity
 import org.bukkit.Bukkit
 
 object UpdateMob {
+    fun recreateGearyEntity(entity: BukkitEntity) {
+        val gearyEntity = entity.toGearyOrNull() ?: return
+        gearyEntity.encodeComponentsTo(entity)
+        gearyEntity.removeEntity()
+        gearyMobs.bukkit2Geary.getOrCreate(entity)
+    }
+
     fun scheduleRecreation(entity: BukkitEntity, gearyEntity: GearyEntity) {
         val loc = entity.location
         val prefab = gearyEntity.prefabs.first()
