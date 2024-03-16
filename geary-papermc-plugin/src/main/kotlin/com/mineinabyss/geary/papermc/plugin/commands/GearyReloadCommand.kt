b68ada469f7c946b28f8e28c65fef54cd4a2d8b8
Danielle Voznyy <dan.voznyy@gmail.com>
2024-03-16 14:24:14 -0400

    cleanup: Remove ConfigBased from PaperEngineModule and use idofront DI for logger

--- geary-papermc-plugin/src/main/kotlin/com/mineinabyss/geary/papermc/plugin/commands/GearyReloadCommand.kt
+++ geary-papermc-plugin/src/main/kotlin/com/mineinabyss/geary/papermc/plugin/commands/GearyReloadCommand.kt
@@ -1,10 +1,8 @@
 package com.mineinabyss.geary.papermc.plugin.commands
 
 import com.mineinabyss.geary.papermc.gearyPaper
-import com.mineinabyss.geary.papermc.plugin.PaperEngineModule
 import com.mineinabyss.geary.prefabs.prefabs
 import com.mineinabyss.idofront.commands.Command
-import com.mineinabyss.idofront.di.DI
 
 private val prefabLoader get() = prefabs.loader
 
@@ -12,7 +10,6 @@ fun Command.reload() {
     "reload" {
         action {
             gearyPaper.configHolder.reload()
-            DI.get<PaperEngineModule>().updateToMatch(gearyPaper.config)
             prefabLoader.loadOrUpdatePrefabs()
         }
     }
