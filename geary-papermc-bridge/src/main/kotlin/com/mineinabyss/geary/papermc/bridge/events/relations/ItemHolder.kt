20f9ec3c6f8cbcfdbb95e7ec3c5e474a5c6b4ccc
Danielle Voznyy <dan.voznyy@gmail.com>
2024-04-20 12:25:42 -0400

    refactor: Start removing bridge module

--- geary-papermc-bridge/src/main/kotlin/com/mineinabyss/geary/papermc/bridge/events/relations/ItemHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.mineinabyss.geary.papermc.bridge.events.relations
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-@SerialName("geary:item_holder")
-sealed class ItemHolder
--- geary-papermc-bridge/src/main/kotlin/com/mineinabyss/geary/papermc/bridge/targetselectors/EmittedTargets.kt
+++
@@ -1,5 +0,0 @@
-package com.mineinabyss.geary.papermc.bridge.targetselectors
-
-import com.mineinabyss.geary.datatypes.GearyEntity
-
-class EmittedTargets(val targets: List<GearyEntity>)
