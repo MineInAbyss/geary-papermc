824d882d12cc2500db4559cbdfcac0c68614874f
Danielle Voznyy <dan.voznyy@gmail.com>
2024-05-12 20:01:27 -0400

    chore: add a way to init a mob spawned from prefab before AttemptSpawn is fired

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/helpers/Helpers.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/entities/helpers/Helpers.kt
@@ -12,19 +12,22 @@ import org.bukkit.Location
 import org.bukkit.persistence.PersistentDataContainer
 
 
-fun Location.spawnFromPrefab(prefab: PrefabKey): Result<BukkitEntity> {
+fun Location.spawnFromPrefab(prefab: PrefabKey, initEntityPreEvent: GearyEntity.() -> Unit = {}): Result<BukkitEntity> {
     val entity = prefabs.manager[prefab] ?: return Result.failure(IllegalArgumentException("No prefab found"))
-    return spawnFromPrefab(entity)
+    return spawnFromPrefab(entity, initEntityPreEvent = initEntityPreEvent)
 }
+
 
 fun Location.spawnFromPrefab(
     prefab: GearyEntity,
     existingPDC: PersistentDataContainer? = null,
+    initEntityPreEvent: GearyEntity.() -> Unit = {}
 ): Result<BukkitEntity> {
     return runCatching {
         val entity = entity {
             if (existingPDC != null) loadComponentsFrom(existingPDC)
             extend(prefab)
+            initEntityPreEvent()
             emit(AttemptSpawn(this@spawnFromPrefab))
         }
         val bukkit = entity.get<BukkitEntity>() ?: error("Entity was not created when spawning from prefab")
