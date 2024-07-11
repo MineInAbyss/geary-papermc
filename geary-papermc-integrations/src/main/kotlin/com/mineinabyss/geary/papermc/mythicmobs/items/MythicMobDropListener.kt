32656aa999926b6116ec17384b1d0f1f0b1b052b
Boy <sivertpaulsen2@gmail.com>
2024-07-11 16:11:20 +0200

    fix: mythicmobs drop issue

--- geary-papermc-integrations/src/main/kotlin/com/mineinabyss/geary/papermc/mythicmobs/items/MythicMobDropListener.kt
+++ geary-papermc-integrations/src/main/kotlin/com/mineinabyss/geary/papermc/mythicmobs/items/MythicMobDropListener.kt
@@ -4,6 +4,7 @@ import com.mineinabyss.geary.papermc.tracking.items.gearyItems
 import com.mineinabyss.geary.papermc.tracking.items.helpers.GearyItemPrefabQuery.Companion.getKeys
 import com.mineinabyss.geary.prefabs.PrefabKey
 import io.lumine.mythic.bukkit.adapters.BukkitItemStack
+import io.lumine.mythic.bukkit.adapters.item.ItemComponentBukkitItemStack
 import io.lumine.mythic.bukkit.events.MythicDropLoadEvent
 import io.lumine.mythic.core.drops.droppables.VanillaItemDrop
 import org.bukkit.event.EventHandler
@@ -15,8 +16,8 @@ class MythicMobDropListener : Listener {
     fun MythicDropLoadEvent.onMythicDropLoad() {
         if (dropName.lowercase() != "geary") return
         val prefabKey = PrefabKey.of(container.line.split(" ")[1])
-        if (prefabKey !in gearyItems.prefabs.getKeys()) return
+        val itemStack = gearyItems.createItem(prefabKey) ?: return
 
-        register(VanillaItemDrop(container.line, config, BukkitItemStack(gearyItems.createItem(prefabKey))))
+        register(VanillaItemDrop(container.line, config, ItemComponentBukkitItemStack(itemStack)))
     }
 }
