2d7178a6e3ac23052509a316db90d0c8ae661fa0
Danielle Voznyy <dan.voznyy@gmail.com>
2024-07-25 17:17:25 -0400

    feat: Add mythic conditions support

--- geary-papermc-integrations/src/main/kotlin/com/mineinabyss/geary/papermc/mythicmobs/events/RunMythicMobsSkills.kt
+++ geary-papermc-integrations/src/main/kotlin/com/mineinabyss/geary/papermc/mythicmobs/events/RunMythicMobsSkills.kt
@@ -2,7 +2,7 @@ package com.mineinabyss.geary.papermc.mythicmobs.events
 
 import com.google.common.collect.Lists
 import com.mineinabyss.geary.modules.GearyModule
-import com.mineinabyss.geary.prefabs.configuration.components.Action
+import com.mineinabyss.geary.prefabs.configuration.components.Condition
 import com.mineinabyss.geary.prefabs.configuration.components.RoleContext
 import com.mineinabyss.geary.serialization.serializers.InnerSerializer
 import com.mineinabyss.geary.systems.builders.observeWithData
@@ -11,7 +11,6 @@ import com.mineinabyss.idofront.typealiases.BukkitEntity
 import io.lumine.mythic.api.adapters.AbstractEntity
 import io.lumine.mythic.bukkit.BukkitAdapter
 import io.lumine.mythic.bukkit.MythicBukkit
-import io.lumine.mythic.core.skills.SkillCondition
 import io.lumine.mythic.core.skills.SkillMetadataImpl
 import io.lumine.mythic.core.skills.SkillTriggers
 import kotlinx.serialization.Serializable
@@ -24,17 +23,37 @@ class RunMythicMobsSkills(
     val keys: List<String>,
 ) {
     class Serializer : InnerSerializer<List<String>, RunMythicMobsSkills>(
-        serialName = "geary:run_skills",
+        serialName = "geary:mythic_skills",
         inner = ListSerializer(String.serializer()),
         inverseTransform = { it.keys },
         transform = ::RunMythicMobsSkills
     )
 }
 
+@Serializable(with = MythicMobsCondition.Serializer::class)
+class MythicMobsCondition(
+    val keys: List<String>,
+) : Condition {
+    class Serializer : InnerSerializer<List<String>, MythicMobsCondition>(
+        serialName = "geary:mythic_conditions",
+        inner = ListSerializer(String.serializer()),
+        inverseTransform = { it.keys },
+        transform = ::MythicMobsCondition
+    )
+
+    override fun RoleContext.execute(): Boolean {
+        val bukkit = entity.get<BukkitEntity>() ?: return true
+        val entity = BukkitAdapter.adapt(bukkit)
+        return keys.all { line ->
+            val condition = MythicBukkit.inst().skillManager.getCondition(line)
+            condition.evaluateEntity(entity)
+        }
+    }
+}
+
 fun GearyModule.runMMSkillAction() = observeWithData<RunMythicMobsSkills>()
     .exec(query<BukkitEntity>()) { (bukkit) ->
-        event.keys.forEach {
-            val line = "[ - $it ]"
+        event.keys.forEach { line ->
             val entity = BukkitAdapter.adapt(bukkit)
             val caster = MythicBukkit.inst().skillManager.getCaster(entity)
             val skill = MythicBukkit.inst().skillManager.getSkill(line).getOrNull()
