ef66642fead413a240806f323dc226991c1b396e
Danielle Voznyy <dan.voznyy@gmail.com>
2024-07-31 12:15:32 -0400

    feat: Add some new actions, including GetNearbyEntities

---
+++ geary-papermc-bridge/src/main/kotlin/com/mineinabyss/geary/papermc/bridge/getters/GetNearbyEntities.kt
@@ -0,0 +1,24 @@
+package com.mineinabyss.geary.papermc.bridge.getters
+
+import com.mineinabyss.geary.actions.Action
+import com.mineinabyss.geary.actions.ActionGroupContext
+import com.mineinabyss.geary.actions.expressions.Expression
+import com.mineinabyss.geary.datatypes.GearyEntity
+import com.mineinabyss.geary.papermc.tracking.entities.toGearyOrNull
+import com.mineinabyss.idofront.typealiases.BukkitEntity
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+@SerialName("geary:get.nearby_entities")
+class GetNearbyEntities(
+    val radius: Expression<Double>,
+) : Action {
+    override fun ActionGroupContext.execute(): List<GearyEntity> {
+        val radius = eval(radius)
+        val bukkit = entity.get<BukkitEntity>() ?: return emptyList()
+        return bukkit
+            .getNearbyEntities(radius, radius, radius)
+            .mapNotNull { it.toGearyOrNull().takeIf { it != entity } }
+    }
+}
