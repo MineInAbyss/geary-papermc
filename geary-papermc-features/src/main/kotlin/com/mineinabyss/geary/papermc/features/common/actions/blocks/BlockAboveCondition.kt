f81cec2886a32d89af0a5a1a437440ac974ed451
Danielle Voznyy <dan.voznyy@gmail.com>
2024-08-22 19:39:59 -0400

    feat: Yaml list $remove merging symbol feat: block and block_above conditions

--- geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/common/actions/blocks/BlockAboveCondition.kt
+++ geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/common/actions/blocks/BlockAboveCondition.kt
@@ -3,17 +3,17 @@ package com.mineinabyss.geary.papermc.features.common.actions.blocks
 import com.mineinabyss.geary.actions.ActionGroupContext
 import com.mineinabyss.geary.actions.Condition
 import com.mineinabyss.geary.papermc.location
-import com.mineinabyss.idofront.location.down
+import com.mineinabyss.idofront.location.up
 import com.mineinabyss.idofront.serialization.MaterialByNameSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.Material
 
 @Serializable
-@SerialName("geary:block_below")
-class BlockBelowCondition(
+@SerialName("geary:block_above")
+class BlockAboveCondition(
     val allow: Set<@Serializable(with = MaterialByNameSerializer::class) Material> = setOf(),
     val deny: Set<@Serializable(with = MaterialByNameSerializer::class) Material> = setOf(),
 ) : Condition {
-    override fun ActionGroupContext.execute(): Boolean = BlockCondition.check(location?.clone()?.down(1), allow, deny)
+    override fun ActionGroupContext.execute(): Boolean = BlockCondition.check(location?.clone()?.up(1), allow, deny)
 }
