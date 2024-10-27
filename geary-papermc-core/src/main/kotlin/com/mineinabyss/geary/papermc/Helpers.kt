43a8bc924c0b5110d4e6dcab0a922ad8ee46d146
Boy <sivertpaulsen2@gmail.com>
2024-10-27 15:36:01 +0100

    refactor: pass initial argument in withGeary block

--- geary-papermc-core/src/main/kotlin/com/mineinabyss/geary/papermc/Helpers.kt
+++ geary-papermc-core/src/main/kotlin/com/mineinabyss/geary/papermc/Helpers.kt
@@ -5,10 +5,13 @@ import com.mineinabyss.geary.prefabs.PrefabKey
 import com.mineinabyss.geary.prefabs.entityOfOrNull
 import com.mineinabyss.idofront.typealiases.BukkitEntity
 import org.bukkit.block.Block
+import org.bukkit.block.TileState
 
 context(Geary)
 fun PrefabKey.toEntityOrNull() = entityOfOrNull(this)
 
-inline fun <T> BukkitEntity.withGeary(run: Geary.() -> T) = with(world.toGeary()) { run() }
+inline fun <T, R : BukkitEntity> R.withGeary(run: Geary.(R) -> T) = with(world.toGeary()) { run(this@withGeary) }
 
-inline fun <T> Block.withGeary(run: Geary.() -> T) = with(world.toGeary()) { run() }
+inline fun <T, R : TileState> R.withGeary(run: Geary.(R) -> T) = with(world.toGeary()) { run(this@withGeary) }
+
+inline fun <T, R : Block> R.withGeary(run: Geary.(R) -> T) = with(world.toGeary()) { run(this@withGeary) }
