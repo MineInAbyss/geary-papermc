7417dd05933330b28afb3f81607e08ea221e461d
Scyu_ <scyu@scyu.dev>
2024-10-28 00:28:42 +0000

    feat: allow resource packs to be added from plugins

--- geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/items/resourcepacks/ResourcePackAddon.kt
+++ geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/items/resourcepacks/ResourcePackAddon.kt
@@ -1,10 +1,12 @@
 package com.mineinabyss.geary.papermc.features.items.resourcepacks
 
+import com.mineinabyss.geary.addons.GearyPhase
 import com.mineinabyss.geary.addons.dsl.GearyDSL
 import com.mineinabyss.geary.addons.dsl.createAddon
 import com.mineinabyss.geary.modules.GearySetup
 import com.mineinabyss.geary.prefabs.PrefabsDSLExtensions
 import com.mineinabyss.geary.prefabs.PrefabsDSLExtensions.walkJarResources
+import team.unnamed.creative.ResourcePack
 import kotlin.reflect.KClass
 
 val ResourcePackAddon = createAddon<ResourcePackConfig, ResourcePackGenerator>(
@@ -12,7 +14,7 @@ val ResourcePackAddon = createAddon<ResourcePackConfig, ResourcePackGenerator>(
     configuration = { ResourcePackConfig() }
 ) {
     val generator = ResourcePackGenerator(geary, configuration)
-    entities {
+    on(GearyPhase.ENABLE) {
         generator.generateResourcePack()
     }
     generator
@@ -20,10 +22,15 @@ val ResourcePackAddon = createAddon<ResourcePackConfig, ResourcePackGenerator>(
 
 data class ResourcePackConfig(
     val jarResources: MutableList<PrefabsDSLExtensions.JarResource> = mutableListOf(),
+    val resourcePacks: MutableList<ResourcePack> = mutableListOf()
 ) {
     fun fromJarResourceDirectory(classLoaderRef: KClass<*>, resource: String) {
         val classLoader = classLoaderRef.java.classLoader
         walkJarResources(classLoader, resource).forEach { jarResources.add(it) }
+    }
+
+    fun addResourcePack(resourcePack: ResourcePack) {
+        resourcePacks.add(resourcePack)
     }
 }
 
