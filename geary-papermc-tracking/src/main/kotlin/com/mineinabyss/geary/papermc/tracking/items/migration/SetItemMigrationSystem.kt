21904cd21a41d6beb1a46865355fc6c19c07def6
Danielle Voznyy <dan.voznyy@gmail.com>
2025-02-01 18:30:31 -0500

    fix: Remove displayName component for geary items with matching display & item names (1.21 migration)

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/items/migration/SetItemMigrationSystem.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/items/migration/SetItemMigrationSystem.kt
@@ -5,10 +5,26 @@ import com.mineinabyss.geary.modules.observe
 import com.mineinabyss.geary.observers.events.OnSet
 import com.mineinabyss.geary.papermc.tracking.items.components.SetItem
 import com.mineinabyss.geary.systems.query.query
+import com.mineinabyss.idofront.items.editItemMeta
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.inventory.ItemStack
+
+private val plainTextSerializer by lazy { PlainTextComponentSerializer.plainText() }
 
 fun Geary.createItemMigrationListener() = observe<OnSet>()
     .involving(query<SetItem, ItemStack>())
     .exec { (setItem, item) ->
         setItem.item.toItemStack(applyTo = item)
+
+        // Migrate display name -> item name
+        val newItem = setItem.item
+        val oldCustomName = item.itemMeta.displayName()
+        val newItemName = newItem.itemName
+
+        // if old item name matches new one, ignoring formatting, remove it
+        if (newItem.customName == null && newItemName != null && oldCustomName != null &&
+            plainTextSerializer.serialize(oldCustomName) == plainTextSerializer.serialize(newItemName)
+        ) item.editItemMeta {
+            displayName(null)
+        }
     }
