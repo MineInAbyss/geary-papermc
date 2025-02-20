2772ac6657ab9a523e9b64371d5f6d2c3ee76303
Boy <sivertpaulsen2@gmail.com>
2025-02-20 17:56:15 +0100

    feat: remove hat & backpack features

--- geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/items/ItemFeatures.kt
+++ geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/items/ItemFeatures.kt
@@ -2,11 +2,9 @@ package com.mineinabyss.geary.papermc.features.items
 
 import com.mineinabyss.geary.papermc.Feature
 import com.mineinabyss.geary.papermc.FeatureContext
-import com.mineinabyss.geary.papermc.features.items.backpack.BackpackListener
 import com.mineinabyss.geary.papermc.features.items.food.ReplaceBurnedDropListener
 import com.mineinabyss.geary.papermc.features.items.holdsentity.SpawnHeldPrefabListener
 import com.mineinabyss.geary.papermc.features.items.nointeraction.DisableItemInteractionsListener
-import com.mineinabyss.geary.papermc.features.items.wearables.WearableItemListener
 import com.mineinabyss.geary.papermc.gearyPaper
 
 class ItemFeatures(context: FeatureContext) : Feature(context) {
@@ -14,8 +12,6 @@ class ItemFeatures(context: FeatureContext) : Feature(context) {
 
     override fun enable() {
         listeners(
-            WearableItemListener(),
-            BackpackListener(),
             SpawnHeldPrefabListener(),
             DisableItemInteractionsListener(),
             ReplaceBurnedDropListener(),
