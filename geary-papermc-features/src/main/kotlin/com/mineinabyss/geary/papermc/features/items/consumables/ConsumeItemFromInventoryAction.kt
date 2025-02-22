48029da9c394095a32aa1f6f3ab6eea8d5fc7e7b
Danielle Voznyy <dan.voznyy@gmail.com>
2025-02-22 13:11:37 -0500

    fix: consumeItem not working in offhand

--- geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/items/consumables/ConsumeItemFromInventoryAction.kt
+++ geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/items/consumables/ConsumeItemFromInventoryAction.kt
@@ -15,7 +15,8 @@ class ConsumeItemFromInventoryAction(
 ) : Action {
     override fun ActionGroupContext.execute() {
         val player = entity?.get<Player>() ?: return
-        val matchedItem = player.inventory.firstOrNull { type.matches(it) } ?: return
+        val matchedItem = player.inventory.filterNotNull().firstOrNull { type.matches(it) }
+            ?: player.inventory.itemInOffHand.takeIf { type.matches(it) } ?: return
         matchedItem.amount -= amount
     }
 }
