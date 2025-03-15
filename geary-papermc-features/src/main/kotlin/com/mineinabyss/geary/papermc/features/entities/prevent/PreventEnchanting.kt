623b23d5a2412f684d817202822cc95be7eb3ed1
Danielle Voznyy <dan.voznyy@gmail.com>
2025-03-14 23:53:57 -0400

    fix: Prevent anvil enchants, and removing via grindstone (infinite xp)

--- geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/entities/prevent/PreventEnchanting.kt
+++ geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/entities/prevent/PreventEnchanting.kt
@@ -4,10 +4,14 @@ import com.mineinabyss.geary.papermc.toGeary
 import com.mineinabyss.geary.papermc.tracking.items.itemEntityContext
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bukkit.World
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.enchantment.EnchantItemEvent
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent
+import org.bukkit.event.inventory.PrepareAnvilEvent
+import org.bukkit.event.inventory.PrepareGrindstoneEvent
+import org.bukkit.inventory.ItemStack
 
 /**
  * Prevents an item from being enchantable in an enchanting table.
@@ -18,19 +22,36 @@ class PreventEnchanting
 
 class PreventEnchantingListener() : Listener {
     @EventHandler
-    fun PrepareItemEnchantEvent.disableEnchantPreview() = with(enchanter.world.toGeary()) {
-        itemEntityContext {
-            if (item.toGearyOrNull()?.has<PreventEnchanting>() == true) {
-                isCancelled = true
-            }
-        }
+    fun PrepareItemEnchantEvent.disableEnchantPreview() {
+        if (shouldPrevent(enchanter.world, item)) isCancelled = true
     }
 
-    fun EnchantItemEvent.disableEnchanting() = with(enchanter.world.toGeary()) {
-        itemEntityContext {
-            if (item.toGearyOrNull()?.has<PreventEnchanting>() == true) {
-                isCancelled = true
+    @EventHandler
+    fun EnchantItemEvent.disableEnchanting() {
+        if (shouldPrevent(enchanter.world, item)) isCancelled = true
+    }
+
+    @EventHandler
+    fun PrepareAnvilEvent.disableAnvilEnchant() {
+        val world = viewers.firstOrNull()?.world ?: return
+        if (shouldPrevent(world, result)) result = null
+    }
+
+    @EventHandler
+    fun PrepareGrindstoneEvent.disableGrindstone() {
+        val world = viewers.firstOrNull()?.world ?: return
+        if (shouldPrevent(world, result)) result = null
+    }
+
+    fun shouldPrevent(world: World, item: ItemStack?): Boolean {
+        if (item == null) return false
+        with(world.toGeary()) {
+            itemEntityContext {
+                if (item.toGearyOrNull()?.has<PreventEnchanting>() == true) {
+                    return true
+                }
             }
         }
+        return false
     }
 }
