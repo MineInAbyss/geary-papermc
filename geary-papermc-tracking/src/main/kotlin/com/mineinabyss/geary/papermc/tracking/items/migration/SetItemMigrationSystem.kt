6bdc39911d0021f4f2a282019517abfde5066bc1
Boy0000 <sivertpaulsen2@gmail.com>
2025-05-18 14:36:16 +0200

    fix: reset cmd after building new item

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/items/migration/SetItemMigrationSystem.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/items/migration/SetItemMigrationSystem.kt
@@ -14,13 +14,6 @@ private val plainTextSerializer by lazy { PlainTextComponentSerializer.plainText
 fun Geary.createItemMigrationListener() = observe<OnSet>()
     .involving(query<SetItem, ItemStack>())
     .exec { (setItem, item) ->
-        val itemModel = setItem.item.takeIf { it.customModelData == null }?.itemModel
-        val oldCmd = item.getData(DataComponentTypes.CUSTOM_MODEL_DATA)
-        val oldItemModel = item.getData(DataComponentTypes.ITEM_MODEL)
-
-        if (itemModel != null && oldCmd != null && oldItemModel != itemModel)
-            item.unsetData(DataComponentTypes.CUSTOM_MODEL_DATA)
-
         setItem.item.toItemStack(applyTo = item)
 
         // Migrate display name -> item name
@@ -30,4 +23,7 @@ fun Geary.createItemMigrationListener() = observe<OnSet>()
         // if old item name matches new one, ignoring formatting, remove it
         if (itemName != null && customName != null && plainTextSerializer.serialize(customName) == plainTextSerializer.serialize(itemName))
             item.unsetData(DataComponentTypes.CUSTOM_NAME)
+
+        if (item.hasData(DataComponentTypes.CUSTOM_MODEL_DATA) && setItem.item.customModelData == null)
+            item.unsetData(DataComponentTypes.CUSTOM_MODEL_DATA)
     }
