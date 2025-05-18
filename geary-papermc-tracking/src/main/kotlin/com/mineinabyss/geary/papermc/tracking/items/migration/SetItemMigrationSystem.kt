37b9bb3bf6a7a9cfd281384fe0f312fd765c6a84
Boy0000 <sivertpaulsen2@gmail.com>
2025-05-18 14:21:17 +0200

    fix: remove cmd from old items if itemmodel isnt specified

--- geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/items/migration/SetItemMigrationSystem.kt
+++ geary-papermc-tracking/src/main/kotlin/com/mineinabyss/geary/papermc/tracking/items/migration/SetItemMigrationSystem.kt
@@ -14,13 +14,20 @@ private val plainTextSerializer by lazy { PlainTextComponentSerializer.plainText
 fun Geary.createItemMigrationListener() = observe<OnSet>()
     .involving(query<SetItem, ItemStack>())
     .exec { (setItem, item) ->
+        val itemModel = setItem.item.takeIf { it.customModelData == null }?.itemModel
+        val oldCmd = item.getData(DataComponentTypes.CUSTOM_MODEL_DATA)
+        val oldItemModel = item.getData(DataComponentTypes.ITEM_MODEL)
+
+        if (itemModel != null && oldCmd != null && oldItemModel != itemModel)
+            item.unsetData(DataComponentTypes.CUSTOM_MODEL_DATA)
+
         setItem.item.toItemStack(applyTo = item)
 
         // Migrate display name -> item name
-        val itemName = setItem.item.takeIf { it.itemName != null && it.customName == null }?.itemName ?: return@exec
-        val customName = item.getData(DataComponentTypes.CUSTOM_NAME) ?: return@exec
+        val itemName = setItem.item.takeIf { it.itemName != null && it.customName == null }?.itemName
+        val customName = item.getData(DataComponentTypes.CUSTOM_NAME)
 
         // if old item name matches new one, ignoring formatting, remove it
-        if (plainTextSerializer.serialize(customName) == plainTextSerializer.serialize(itemName))
+        if (itemName != null && customName != null && plainTextSerializer.serialize(customName) == plainTextSerializer.serialize(itemName))
             item.unsetData(DataComponentTypes.CUSTOM_NAME)
     }
