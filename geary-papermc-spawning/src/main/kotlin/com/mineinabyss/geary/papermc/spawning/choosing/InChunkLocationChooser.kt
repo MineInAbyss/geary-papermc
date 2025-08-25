4773d460d5e08be13c7bdefea53910f0afdfff75
Boy0000 <sivertpaulsen2@gmail.com>
2025-08-25 17:14:40 +0200

    refactor: refactorio

--- geary-papermc-spawning/src/main/kotlin/com/mineinabyss/geary/papermc/spawning/choosing/InChunkLocationChooser.kt
+++ geary-papermc-spawning/src/main/kotlin/com/mineinabyss/geary/papermc/spawning/choosing/InChunkLocationChooser.kt
@@ -1,7 +1,6 @@
 package com.mineinabyss.geary.papermc.spawning.choosing
 
 import com.mineinabyss.geary.papermc.spawning.MobSpawner
-import com.mineinabyss.geary.papermc.spawning.config.SpawnEntry
 import com.mineinabyss.geary.papermc.spawning.config.SpreadSpawnConfig
 import com.mineinabyss.geary.papermc.spawning.helpers.launchWithTicket
 import com.mineinabyss.geary.papermc.spawning.readers.SpawnPositionReader
@@ -15,38 +14,34 @@ class InChunkLocationChooser(
 ) {
 
     suspend fun chooseSpotInChunk(chunkLoc: Location, config: SpreadSpawnConfig): Location? {
+        if (!chunkLoc.isWorldLoaded || !chunkLoc.isChunkLoaded || chunkLoc.world != mainWorld) return null
         val chunk = chunkLoc.chunk
-        if (!chunkLoc.isWorldLoaded || !chunkLoc.isChunkLoaded || chunkLoc.world != mainWorld)
-            return null
-        val pos = chunk.launchWithTicket {
+
+        return chunk.launchWithTicket {
             repeat(config.spawnAttempts) {
                 val candidate = getValidBlockOrNull(chunk, config)
                 if (candidate != null)
                     return@launchWithTicket candidate
             }
             return@launchWithTicket null
-        }
-        return pos.await()
+        }.await()
     }
 
     private fun getValidBlockOrNull(chunk: Chunk, config: SpreadSpawnConfig): Location? {
-        val spawnPositionReader = SpawnPositionReader()
-        val testloc = getRandomChunkCoord(chunk, config)
-        val type = spawnPositionReader.spawnPositionFor(testloc)
-        val entry: SpawnEntry = config.entry
+        val location = getRandomChunkCoord(chunk, config)
+        val type = SpawnPositionReader.spawnPositionFor(location)
 
-        if (type != entry.position || !mobSpawner.checkSpawnConditions(entry, testloc))
+        if (type != config.entry.position || !mobSpawner.checkSpawnConditions(config.entry, location))
             return null
 
-        return testloc
+        return location
     }
 
     // chose a random spot within the chunk
     private fun getRandomChunkCoord(chunk: Chunk, config: SpreadSpawnConfig): Location {
-        val yRange = config.sectionMinY.. config.sectionMaxY
         val x = chunk.x * 16 + (0..15).random()
         val z = chunk.z * 16 + (0..15).random()
-        val y = yRange.random()
+        val y = (config.sectionMinY.. config.sectionMaxY).random()
         return Location(mainWorld, x.toDouble(), y.toDouble(), z.toDouble())
     }
 }
