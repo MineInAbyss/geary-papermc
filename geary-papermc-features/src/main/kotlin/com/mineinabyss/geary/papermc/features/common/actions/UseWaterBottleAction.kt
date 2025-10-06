b4a76ea457f38a96fcc9fcb76f4eee5f866fdbe6
Helia <soulhood2.0@gmail.com>
2025-10-06 19:32:25 +0200

    feat: infinite water bottle action

---
+++ geary-papermc-features/src/main/kotlin/com/mineinabyss/geary/papermc/features/common/actions/UseWaterBottleAction.kt
@@ -0,0 +1,49 @@
+package com.mineinabyss.geary.papermc.features.common.actions
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import com.mineinabyss.geary.actions.Action
+import com.mineinabyss.geary.actions.ActionGroupContext
+import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldguard.WorldGuard
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin
+import org.bukkit.Material
+import org.bukkit.block.data.Levelled
+import org.bukkit.entity.Player
+
+@Serializable
+@SerialName("geary:water_bottle")
+class UseWaterBottleAction(
+    val cauldronLevelIncrease: Int = 1
+): Action {
+    override fun ActionGroupContext.execute() {
+        println("bottle: used")
+        val player = entity?.get<Player>() ?: return println("bottle: no player")
+        val targetedBlock = player.rayTraceBlocks(3.0)?.hitBlock ?: return println("bottle: no block")
+        val container = WorldGuard.getInstance().platform.regionContainer
+        val query = container.createQuery()
+        val location = BukkitAdapter.adapt(targetedBlock.location)
+
+        val authorized = query.testState(location, WorldGuardPlugin.inst().wrapPlayer(player), com.sk89q.worldguard.protection.flags.Flags.BUILD)
+        if (!authorized) return println("bottle: no permission")
+        if (targetedBlock.type == Material.CAULDRON) {
+            targetedBlock.type = Material.WATER_CAULDRON
+            val cauldronData = targetedBlock.blockData
+            if (cauldronData is Levelled) {
+                cauldronData.level = cauldronLevelIncrease
+                targetedBlock.blockData = cauldronData
+            }
+        } else if (targetedBlock.type == Material.WATER_CAULDRON) {
+            val cauldronData = targetedBlock.blockData
+            if (cauldronData is Levelled) {
+                val new_level = if (cauldronData.level + cauldronLevelIncrease > 3) 3 else cauldronData.level + cauldronLevelIncrease
+                cauldronData.level = new_level
+                targetedBlock.blockData = cauldronData
+            }
+        }
+
+        if (targetedBlock.type == Material.DIRT) {
+            targetedBlock.type = Material.MUD
+        }
+    }
+}
