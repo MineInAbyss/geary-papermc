f649c00ef8c418e0b3c684b0753bbaa8f9c54be7
Danielle Voznyy <dan.voznyy@gmail.com>
2026-05-30 11:40:55 -0400

    chore: Migrate idofrontLibs -> miaLibs

--- geary-papermc-tracking/build.gradle.kts
+++ geary-papermc-tracking/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    id(idofrontLibs.plugins.mia.kotlin.jvm.get().pluginId)
-    id(idofrontLibs.plugins.mia.papermc.get().pluginId)
-    id(idofrontLibs.plugins.mia.nms.get().pluginId)
-    id(idofrontLibs.plugins.mia.publication.get().pluginId)
-    alias(idofrontLibs.plugins.kotlinx.serialization)
+    id(miaLibs.plugins.mia.kotlin.jvm.get().pluginId)
+    id(miaLibs.plugins.mia.papermc.get().pluginId)
+    id(miaLibs.plugins.mia.nms.get().pluginId)
+    id(miaLibs.plugins.mia.publication.get().pluginId)
+    alias(miaLibs.plugins.kotlinx.serialization)
 }
 
 dependencies {
-    compileOnly(idofrontLibs.idofront.nms)
-    compileOnly(idofrontLibs.idofront.services)
-    compileOnly(idofrontLibs.minecraft.mccoroutine)
+    compileOnly(miaLibs.idofront.nms)
+    compileOnly(miaLibs.idofront.services)
+    compileOnly(miaLibs.minecraft.mccoroutine)
     implementation(libs.geary.uuid)
     api(project(":geary-papermc-datastore"))
     api(project(":geary-papermc-core"))
