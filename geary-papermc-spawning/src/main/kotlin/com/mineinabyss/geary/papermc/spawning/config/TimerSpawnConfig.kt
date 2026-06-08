732e90d8ec55de9e56faee0dfb698a44fe0a50b9
Helia8 <126818312+Helia8@users.noreply.github.com>
2026-06-08 06:28:12 +0200

    fix: fixed plugin errors

--- geary-papermc-spawning/src/main/kotlin/com/mineinabyss/geary/papermc/spawning/config/TimerSpawnConfig.kt
+++ geary-papermc-spawning/src/main/kotlin/com/mineinabyss/geary/papermc/spawning/config/TimerSpawnConfig.kt
@@ -2,6 +2,7 @@ package com.mineinabyss.geary.papermc.spawning.config
 
 class TimerSpawnConfig(
     val entry: SpawnEntry,
-
+///execute in minecraft:overworld run tp @s 229100.92 87.74 292.68 -29.18 0.64
+    ///execute in minecraft:overworld run tp @s 229271.48 135.73 486.42 -200.18 19.39
 ) {
 }
